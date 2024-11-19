15f93bb64ce7c49d1b429cfe3cd0628b5f499d3b
Brandon Rock <brandon@rockfam.org>
2024-11-19 06:35:26 -0700

    Added OAuth endpoint method bodies

---
+++ src/OIdentNetLib/Endpoints/Metadata/RequestMetadataCreator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using OIdentNetLib.Application.Common;
+
+namespace OIdentNetLib.Endpoints.Metadata;
+
+public static class RequestMetadataCreator
+{
+    public static RequestMetadata Create(HttpRequest httpRequest)
+    {
+        string? host = httpRequest.Headers["X-Forwarded-For"];
+        if (string.IsNullOrEmpty(host))
+        {
+            host = httpRequest.Host.Value;
+        }
+        
+        return new RequestMetadata()
+        {
+            Host = host
+        };
+    }
+}
--- src/OIdentNetLib/Endpoints/OAuth/OAuthEndpoints.cs
+++ src/OIdentNetLib/Endpoints/OAuth/OAuthEndpoints.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OIdentNetLib.Application.OAuth.Contracts;
+using OIdentNetLib.Application.OAuth.DataTransferObjects;
+using OIdentNetLib.Application.OAuth.Models;
+using OIdentNetLib.Endpoints.Metadata;
 
 namespace OIdentNetLib.Endpoints.OAuth;
 
@@ -7,23 +12,68 @@ public static class OAuthEndpoints
 {
     public static void MapOAuthEndpoints(this WebApplication app)
     {
-        app.MapPost("/oauth/authorize", AuthorizeAsync);
-        app.MapPost("/oauth/token", TokenAsync);
+        app.MapGet("/oauth/authorize", AuthorizeAsync)
+            .WithName("Authorize")
+            .WithDescription("OAuth authorization endpoint.");
+        
+        app.MapPost("/oauth/token", TokenAsync)
+            .WithName("Token")
+            .WithDescription("OAuth token endpoint.");
     }
 
     private static async Task<IResult> AuthorizeAsync(
-        HttpContext context,
-        HttpContent content)
+        HttpContext httpContext,
+        HttpRequest httpRequest,
+        IAuthorizationProcessor authorizationProcessor,
+        [FromQuery] ProcessAuthorizationRequest processAuthorizationRequest)
     {
-        await Task.CompletedTask;
-        return Results.Ok();
+        var requestMetadata = RequestMetadataCreator.Create(httpRequest);
+        var processAuthorizationResponse = await authorizationProcessor.ProcessAsync(
+            requestMetadata,
+            processAuthorizationRequest,
+            new ValidateSessionRequest());
+        return processAuthorizationResponse.ToHttpResult();
     }
 
     private static async Task<IResult> TokenAsync(
-        HttpContext context,
-        HttpContent content)
+        HttpContext httpContext,
+        HttpRequest httpRequest,
+        ITokenProcessor tokenProcessor)
     {
-        await Task.CompletedTask;
-        return Results.Ok();
+        ProcessTokenRequest? processTokenRequest = null;
+
+        if (httpRequest.HasJsonContentType())
+        {
+            processTokenRequest = await httpRequest.ReadFromJsonAsync<ProcessTokenRequest>();
+        }
+        else if (httpRequest.HasFormContentType)
+        {
+            var form = await httpRequest.ReadFormAsync();
+            
+            if (!Uri.TryCreate(form["redirect_uri"], UriKind.Absolute, out var redirectUri))
+            {
+                return Results.BadRequest(OAuthErrorTypes.InvalidRequest);
+            }
+            
+            processTokenRequest = new ProcessTokenRequest()
+            {
+                ClientId = form["client_id"],
+                ClientSecret = form["client_secret"],
+                GrantType = form["grant_type"],
+                Code = form["code"],
+                RedirectUri = redirectUri
+            };
+        }
+        
+        if (processTokenRequest is null)
+        {
+            return Results.BadRequest(OAuthErrorTypes.InvalidRequest);
+        }
+        
+        var requestMetadata = RequestMetadataCreator.Create(httpRequest);
+        var processTokenResponse = await tokenProcessor.ProcessAsync(
+            requestMetadata,
+            processTokenRequest);
+        return processTokenResponse.ToHttpResult();
     }
 }
